8cc94ead465701c0a55c1a8eb4053e97087e5e51
Lucas <lucas.ali3ns@gmail.com>
2026-05-06 10:37:28 -0300

    Add footer to global header

---
+++ Site_QuimeraGames/header_global/footer.php
@@ -0,0 +1,8 @@
+<footer class="rodape-universal">
+    <div class="footer-content">
+        <p>QuimeraGames &copy; 2026 - Todos os direitos reservados.</p>
+        <nav class="footer-links">
+            <a href="../Sobre_Nos/Sobre.php">Sobre Nós</a>
+        </nav>
+    </div>
+</footer>
