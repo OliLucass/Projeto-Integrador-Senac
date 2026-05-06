904d0a4c453ced014af88f1c0e081348e27c95de
Lucas <lucas.ali3ns@gmail.com>
2026-05-06 10:38:42 -0300

    Add simple header for QuimeraGames site

---
+++ Site_QuimeraGames/header_global/header_simples.php
@@ -0,0 +1,5 @@
+<header class="topo-simples">
+    <a href="../Index/index.php" class="logo-link">
+        <img class="logo-auth" src="../imagens/logo.png" alt="QuimeraGames Logo">
+    </a>
+</header>
