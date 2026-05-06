6c9cc6a4fa5cbe831b7f1843a7f6a16238039ec2
Lucas <lucas.ali3ns@gmail.com>
2026-05-06 08:13:54 -0300

    Update Suporte.php with new styles and structure

--- Site_QuimeraGames/SAC/Suporte.php
+++ Site_QuimeraGames/SAC/Suporte.php
@@ -1,64 +1,61 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Suporte ao Cliente</title>
-    <link rel="stylesheet" href="Suporte.css">
+    <title>Suporte ao Cliente - QuimeraGames</title>
+    <link rel="stylesheet" href="../css/global.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="Suporte.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="icon" type="image/x-icon" href="/GitHub/ProjIntegrador/Site_QuimeraGames/favicon.ico">
 </head>
 
 <body>
-    <header class="topo">
-        <div class="topo-esquerda">
-            <a href="http://localhost/GitHub/ProjIntegrador/Site_QuimeraGames/Index/index.php"><img class="logo"
-                src="../imagens/logo.png"></a>
+
+    <?php include '../header_footer_global/header_simples.php'; ?>
+
+    <main class="main-suporte">
+        <div class="card-suporte">
+            <h1>Suporte ao Cliente</h1>
+
+            <div id="blocoErro" class="bloco-erro" style="display:none;">
+                <ul id="listaErros"></ul>
+            </div>
+
+            <form id="formSuporte" action="enviar.php" method="POST">
+
+                <div class="input-group">
+                    <input type="text" name="nome" id="name" required placeholder=" " autocomplete="off">
+                    <label>Nome</label>
+                </div>
+
+                <div class="input-group">
+                    <input type="email" name="email" id="email" required placeholder=" " autocomplete="off">
+                    <label>Email</label>
+                </div>
+
+                <div class="input-group">
+                    <input type="text" name="cpf" id="cpf" maxlength="14" required placeholder=" " autocomplete="off">
+                    <label>CPF</label>
+                </div>
+
+                <div class="input-group textarea-group">
+                    <textarea name="reclamacao" id="reclamacao" required placeholder=" "></textarea>
+                    <label>Escreva sua reclamação/sugestão aqui...</label>
+                    <button type="submit" id="bntContinuar" class="btnEnviar">➤</button>
+                </div>
+
+                <div class="footer-voltar">
+                    <button type="button" class="btnVoltar" onclick="window.history.back()">❮</button>
+                    <span class="labelVoltar" onclick="window.history.back()">Voltar</span>
+                </div>
+
+            </form>
         </div>
-    </header>
+    </main>
 
-    <div class="container">
-        <h1>Suporte ao cliente</h1>
-
-        <div id="blocoErro" class="bloco-erro" style="display:none;">
-            <ul id="listaErros"></ul>
-        </div>
-
-        <form class="item" id="formSuporte" action="enviar.php" method="POST">
-            <div>
-                <label class="label">Nome</label>
-                <input type="text" name="nome" id="name" class="inputNome" required><br>
-            </div>
-
-            <div>
-                <label class="label">Email</label>
-                <input type="email" name="email" id="email" class="inputEmail" required><br>
-            </div>
-
-            <div>
-                <label class="label">CPF</label>
-                <input type="text" name="cpf" id="cpf" class="inputCpf" maxlength="14" required><br>
-            </div>
-
-            <label for="reclamacao" class="label"> Escreva a reclamação/sugestão no campo abaixo: </label><br>
-            <div class="inputContainer">
-                <textarea name="reclamacao" id="reclamacao" class="inputReclamacao" required></textarea>
-                <button type="submit" id="bntContinuar" class="btnEnviar">➤</button>
-            </div>
-
-        </form>
-
-        <footer class="rodape">
-            
-            <a class="btnVoltar" href="../Usuario_Logado/usuariologado.php">
-                <i class="fa-solid fa-circle-arrow-left"></i>
-            </a>
-                
-            <label id="voltar" class="labelVoltar"> Voltar </label>
-            
-        </footer>
-
-    </div>
+    <?php include '../header_footer_global/footer.php'; ?>
 
     <div id="modalSucesso" class="modal-overlay">
         <div class="modal-content">
@@ -66,12 +63,12 @@
                 <i class="fa-solid fa-circle-check"></i>
             </div>
             <h2>Solicitação Enviada</h2>
-            <p>Seu protocolo é: <br> <strong id="numeroProtocolo"></strong></p>
-            <button id="btnFecharModal">OK</button>
+            <p>Seu protocolo de atendimento é: <br><strong id="numeroProtocolo"></strong></p>
+            <button id="btnFecharModal">Entendi</button>
         </div>
     </div>
 
-    <script src="Suporte.js" defer></script>
+    <script src="../js/Suporte.js" defer></script>
 </body>
 
 </html>
