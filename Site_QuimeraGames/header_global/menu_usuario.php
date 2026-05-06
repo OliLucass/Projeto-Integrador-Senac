bc57c698eec9af76850ff379d7f154b5e9cab8aa
Lucas <lucas.ali3ns@gmail.com>
2026-05-06 10:39:23 -0300

    Add user menu with account and wishlist links

--- Site_QuimeraGames/header_global/menu_usuario.php
+++ Site_QuimeraGames/header_global/menu_usuario.php
@@ -1,1 +1,12 @@
-
+<div id="user-menu-2" class="user-menu">
+    <a href="../Conta/conta.php">Conta</a>
+    <a href="../Pagamento/pagamento.php">Pagamento</a>
+    <a href="../Usuario_Logado/wishlist.php">
+        Lista de desejo
+        <?php if (isset($qtd_wishlist) && $qtd_wishlist > 0): ?>
+            <span class="badge-bolinha"><?php echo $qtd_wishlist; ?></span>
+        <?php endif; ?>
+    </a>
+    <a href="../Usuario_Logado/meus_pedidos.php">Meus Pedidos</a>
+    <a href="../Usuario_Logado/logout.php">Sair</a>
+</div>
